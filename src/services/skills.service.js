8842b50885050e6b9f57e7afa4f4780b8a272e2e
rhizene <ncdimalanta@gmail.com>
2024-05-03 12:31:56 -0700

    refactored skills.service

--- src/services/skills.service.js
+++ src/services/skills.service.js
@@ -1,31 +1,15 @@
-import { createClient } from '@supabase/supabase-js';
+import Service from './service';
 
-const api = {
-  url: process.env.REACT_APP_API_URL,
-  key: process.env.REACT_APP_API_KEY,
-};
-const client = createClient(api.url, api.key);
-const state = {
-  gettingSkills: false,
-};
-
-export async function fetchSkills() {
-  if (state.gettingSkills) return Promise.resolve({});
-
-  state.gettingSkills = true;
-  let { data, error } = await client
+export const fetchSkills = new Service(async function () {
+  let { data, error } = await this.client
     .from('skills')
     .select('name, description')
     .neq('description', null);
 
-  if (error) {
-    ;
-    throw error;
-  }
+  if (error) throw error;
 
   const skills = {};
   data.forEach((item) => (skills[item.name] = item.description));
 
-  state.gettingSkills = false;
   return skills;
-}
+}, {});
