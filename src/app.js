0e415e33fffcd5031ebbbd57a48829ef505c1297
rhizene <ncdimalanta@gmail.com>
2024-05-03 09:01:43 -0700

    migrated App.js Component to function

--- src/app.js
+++ src/app.js
@@ -1,67 +1,91 @@
-require.context('./assets', true)
-import "./app.scss"
-import React from "react";
-import Section from "./components/section/section";
-import DetailList from "./components/detailList/detailList";
-import MediaButton from "./components/mediaButton/mediaButton";
+require.context('./assets', true);
+import { useEffect } from 'react';
+import './app.scss';
 import sound from './assets/545883_Shades-Of-James.mp3';
-import IntroDialog from "./components/introDialog/introDialog";
+import DetailList from './components/detailList/detailList';
+import IntroDialog from './components/introDialog/introDialog';
+import MediaButton from './components/mediaButton/mediaButton';
+import Section from './components/section/section';
 
-
+let state = {};
 const skills = {
-    "javascript"               : <>I used <em>Angular</em> and <em>Typescript</em> in my past projects. Recently learning React</>,
-    "NodeJS"                   : <>As a build tool with <em>Webpack</em> or <em>Cordova</em>, or as a backend with frameworks like <em>ExpressJS</em>, I've used Node to create Web and Hybrid Mobile Apps</>,
-    "Stylesheets"              : <>can be pre-compiled with <em>SASS</em>, or guided with frameworks like <em>Bootstrap</em>.</>,
-    "Test Driven Development"  : <><em>Mocha JS</em> + <em>Chai</em>, <em>Sinon</em></>,
+  javascript: (
+    <>
+      I used <em>Angular</em> and <em>Typescript</em> in my past projects.
+      Recently learning React
+    </>
+  ),
+  NodeJS: (
+    <>
+      As a build tool with <em>Webpack</em> or <em>Cordova</em>, or as a backend
+      with frameworks like <em>ExpressJS</em>, I've used Node to create Web and
+      Hybrid Mobile Apps
+    </>
+  ),
+  Stylesheets: (
+    <>
+      can be pre-compiled with <em>SASS</em>, or guided with frameworks like{' '}
+      <em>Bootstrap</em>.
+    </>
+  ),
+  'Test Driven Development': (
+    <>
+      <em>Mocha JS</em> + <em>Chai</em>, <em>Sinon</em>
+    </>
+  ),
+};
+
+function updateTitleState() {
+  const { titleIndex, title, incrementor } = state;
+
+  let nextIncrementor = incrementor;
+  if (titleIndex > title.length) {
+    nextIncrementor = -1;
+  } else if (titleIndex < 0) {
+    nextIncrementor = 1;
+  }
+
+  const nextIndex = titleIndex + incrementor;
+  state = {
+    ...state,
+    titleIndex: nextIndex,
+    incrementor: nextIncrementor,
+  };
 }
 
-class App extends React.Component{
-    constructor(props){
-        super(props);
-        this.state = { 
-            title: 'N I C H O L ',
-            titleIndex: 0,
-            incrementor: 1,
-        };
-    }
-
-    render() {
-        return (<main>
-            <Section title="About" description="A simple landing page converted to React." />
-            <Section title="Skills" >
-                <DetailList listItems={skills}></DetailList>
-            </Section>
-            <MediaButton bgm={sound}></MediaButton>
-            <IntroDialog></IntroDialog>
-        </main>);
-    }
-
-    #updateTitleState(){
-        const {titleIndex, title, incrementor} = this.state;
-
-        let nextIncrementor = incrementor;
-        if(titleIndex > title.length) {
-            nextIncrementor = -1;
-        } else if (titleIndex < 0) {
-            nextIncrementor = 1
-        }
-
-        const nextIndex = titleIndex + incrementor;
-        this.state = {...this.state,
-            titleIndex: nextIndex,
-            incrementor: nextIncrementor};
-    }
-
-    #getAppTitle(){
-        const endIndex = this.state.titleIndex;
-        const title = this.state.title;
-        return title.substring(0, endIndex);
-    }
-
-    renderTitle(){
-        document.title = this.#getAppTitle();
-        this.#updateTitleState();
-    }
+function getAppTitle() {
+  const endIndex = state.titleIndex;
+  const title = state.title;
+  return title.substring(0, endIndex);
 }
 
-export default App;
+function renderTitle() {
+  document.title = getAppTitle();
+  updateTitleState();
+}
+
+export default function App() {
+  useEffect(() => {
+    state = {
+      title: 'N I C H O L ',
+      titleIndex: 0,
+      incrementor: 1,
+    };
+
+    setInterval(() => renderTitle(), 200);
+  }, []);
+
+  return (
+    <main>
+      <Section
+        title="About"
+        description="A simple landing page converted to React."
+      />
+      <Section title="Skills">
+        <DetailList listItems={skills}></DetailList>
+      </Section>
+      <MediaButton bgm={sound}></MediaButton>
+      <IntroDialog></IntroDialog>
+    </main>
+  );
+}
